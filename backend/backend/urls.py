045637e1340bbbc799ce4fd717ea4d72f540442d
Nabeel <nabeeln1230@gmail.com>
2026-01-28 15:53:53 +0530

    Fix dotenv load error

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({"message": "Backend is running!"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('', api_root),
 ]
